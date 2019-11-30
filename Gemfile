2c44a92b205101f999d570bb53c6dd6b47c9eb5b
Douglas Lara <douglasdelaras@gmail.com>
2019-11-30 09:41:14 -0300

    Add Procfile to deploy, add sidekiq scheduler gem, update service notification

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'sidekiq', '~> 5.2'
+gem 'sidekiq-scheduler'
 gem 'httparty'
 gem 'devise'
 gem 'nokogiri'
--- app/services/pucpr_service.rb
+++ app/services/pucpr_service.rb
@@ -2,9 +2,18 @@ class PucprService
   include HTTParty
 
   def get_notes(user)
-    response = HTTParty.get('https://portal.pucpr.br/vda/pages/consultanotas/consultanotas.seam', headers: {"Cookie" => user.cookies} )
-    parsed_data = Nokogiri::HTML.parse(response.body)
-    {result: true, body: parsed_data.xpath('//*[@id="consultaNotasAlunoForm:idTableProgramasAprendizagemAluno"]').to_s }
+    begin
+      response = HTTParty.get('https://portal.pucpr.br/vda/pages/consultanotas/consultanotas.seam', headers: {"Cookie" => user.cookies} )
+      parsed_data = Nokogiri::HTML.parse(response.body)
+      result = parsed_data.xpath('//*[@id="consultaNotasAlunoForm:idTableProgramasAprendizagemAluno"]').to_s
+      if (result != "")
+        return {result: true, body: result }
+      else
+        return {result: false }
+      end 
+    rescue => exception
+      return {result: false }
+    end
   end
 
 end
